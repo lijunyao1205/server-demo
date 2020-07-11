96292fd707e732dcaab7a1a642ec68dbc09d2b53
lijy1219 <463142616@qq.com>
2020-07-11 20:19:12 +0800

    2019 同上

--- config/config.default.js
+++ config/config.default.js
@@ -10,25 +10,25 @@ module.exports = appInfo => {
   config.middleware = [];
 
   // add mysql config here
-  /*config.mysql = {
-    // 单数据库信息配置
-    client: {
-      // host
-      host: '127.0.0.1',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'root',
-      // 密码
-      password: 'root',
-      // 数据库名
-      database: 'vue_vant_demo',
-    },
-    // 是否加载到 app 上，默认开启
-    app: true,
-    // 是否加载到 agent 上，默认关闭
-    agent: false,
-  };*/
+  // config.mysql = {
+  //   // 单数据库信息配置
+  //   client: {
+  //     // host
+  //     host: '127.0.0.1',
+  //     // 端口号
+  //     port: '3306',
+  //     // 用户名
+  //     user: 'root',
+  //     // 密码
+  //     password: 'root',
+  //     // 数据库名
+  //     database: 'vue_vant_demo',
+  //   },
+  //   // 是否加载到 app 上，默认开启
+  //   app: true,
+  //   // 是否加载到 agent 上，默认关闭
+  //   agent: false,
+  // };
 
   // add your user config here
   const userConfig = {
