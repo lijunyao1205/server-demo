4eb3d05b2a18ddcc2e13d6960476dd826e32fbaa
lijy1219 <463142616@qq.com>
2020-07-11 20:29:39 +0800

    同上2029

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,11 +4,11 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
+    // const { ctx } = this;
     // const { ctx, app } = this;
     // const res = await app.mysql.select('article');
     // console.log('res:' + res);
-    ctx.body = 'hi, egg,哈喽，world！';
+    // ctx.body = 'hi, egg,哈喽，world！';
   }
 }
 
