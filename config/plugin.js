40ec5e8fad14d1c24624fe0870be9c5a8e40ce94
lijy1219 <463142616@qq.com>
2020-07-11 20:08:16 +0800

    同上2008

--- config/plugin.js
+++ config/plugin.js
@@ -1,17 +1,5 @@
 'use strict';
 
-/** @type Egg.EggPlugin 
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
- 
-  // mysql : {
-  //   enable: true,
-  //   package: 'egg-mysql',
-  // }
-};*/
 /**开启mysql插件 方式二，需要注释上边*/
 exports.mysql = {
   enable: true,
